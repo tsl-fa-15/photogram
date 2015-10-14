b301a6a55bc286ddbf6a8b2e944ac6f6eb350ddc
Arjun Venkataswamy <avenkat2@gmail.com>
2015-10-14 10:19:33 -0500

    adds http verbs and path helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
 
   # Routes to CREATE photos
-  get("/photos/new", { :controller => "photos", :action => "new_form" })
-  post("/create_photo", { :controller => "photos", :action => "create_row" })
+  get "/photos/new", :controller => "photos", :action => "new_form", :as => 'new_photo'
+  post "/photos",  :controller => "photos", :action => "create_row", :as => 'photos'
 
   # Routes to READ photos
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
-  get('/photos', { :controller => 'photos', :action => "index"})
+  get "/photos/:id",        :controller => "photos", :action => "show", :as => 'photo'
+  get '/photos',  :controller => 'photos', :action => "index"
 
   # Routes to UPDATE photos
-  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
-  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
+  get "/photos/:id/edit",  :controller => "photos", :action => "edit_form", :as => 'edit_photo'
+  patch "/photos/:id",  :controller => "photos", :action => "update_row"
 
   # Route to DESTROY photos
-  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+  delete "/photos/:id",  :controller => "photos", :action => "destroy"
 end
