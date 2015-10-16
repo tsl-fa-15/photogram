9d9f2dff63e878b9619a0c028d7d5a86b9199d2a
Arjun Venkataswamy <avenkat2@gmail.com>
2015-10-16 11:02:27 -0500

    adds error messages to new form

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,12 @@
 class Photo < ActiveRecord::Base
+  validates :caption, presence: true
+  validates :source, presence: true
+  validates :source, length: { maximum: 500 }
+  validate :source_must_be_jpg_or_png
+
+  def source_must_be_jpg_or_png
+    if source[-3..-1] != 'jpg' && source[-3..-1] != 'png'
+      errors.add(:image_format, 'must be jpg or png')
+    end
+  end
 end
